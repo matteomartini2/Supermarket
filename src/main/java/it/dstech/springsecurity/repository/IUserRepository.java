064a08ad4bcbb6165fd49041bce0b2fb30d79e0d
matteo <matteounix@gmail.com>
2018-06-18 17:01:49 +0200

    create  CustomUserDetailsService, security WebSecurityConfig

--- src/main/java/it/dstech/springsecurity/repository/IUserRepository.java
+++ src/main/java/it/dstech/springsecurity/repository/IUserRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import it.dstech.springsecurity.model.User;
 
 public interface IUserRepository extends CrudRepository<User, Long>{
+	
+	public User findByUsername(String username);
 
 }
