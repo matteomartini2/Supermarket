6d02375774aae75201e20d8b813094747efbf6bf
matteo <matteounix@gmail.com>
2018-06-18 16:44:10 +0200

    create repository ICartaCreditoRepository,IProdottoRepository

---
+++ src/main/java/it/dstech/springsecurity/repository/ICartaCreditoRepository.java
@@ -0,0 +1,10 @@
+package it.dstech.springsecurity.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.dstech.springsecurity.model.CartaCredito;
+
+public interface ICartaCreditoRepository  extends CrudRepository<CartaCredito, Long>{
+	
+
+}
---
+++ src/main/java/it/dstech/springsecurity/repository/IProdottoRepository.java
@@ -0,0 +1,9 @@
+package it.dstech.springsecurity.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.dstech.springsecurity.model.Prodotto;
+
+public interface IProdottoRepository  extends CrudRepository<Prodotto, Long>{
+
+}
