2da25538657c410f5cba9a86e0e81a5ada16809c
Veshsare <lowe.blues.lb@gmail.com>
2018-06-19 11:27:48 +0200

    fixed StoricoCtrl

--- src/main/java/it/dstech/springsecurity/api/StoricoCtrl.java
+++ src/main/java/it/dstech/springsecurity/api/StoricoCtrl.java
@@ -1,5 +1,7 @@
 package it.dstech.springsecurity.api;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,7 +27,7 @@ public class StoricoCtrl {
 		}
 		
 		@PostMapping("/many")
-		public Iterable<Storico> createMany(@RequestBody Iterable<Storico> storici){
+		public Iterable<Storico> createMany(@RequestBody List<Storico> storici){
 			return serviceStorico.create(storici);
 		}
 		
