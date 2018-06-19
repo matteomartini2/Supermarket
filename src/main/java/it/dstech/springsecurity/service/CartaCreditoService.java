bf01a2d07cd95260be23298ac6d24ff4a8f125ef
matteo <matteounix@gmail.com>
2018-06-19 14:44:15 +0200

    fix

--- src/main/java/it/dstech/springsecurity/service/CartaCreditoService.java
+++ src/main/java/it/dstech/springsecurity/service/CartaCreditoService.java
@@ -4,34 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import it.dstech.springsecurity.model.CartaCredito;
 import it.dstech.springsecurity.model.User;
 import it.dstech.springsecurity.repository.ICartaCreditoRepository;
+import it.dstech.springsecurity.repository.IUserRepository;
 
 @Service
 public class CartaCreditoService {
 	
-	@Autowired
 	private ICartaCreditoRepository dao;
-	
-	@Autowired
-	private UserService userService;
+	private IUserRepository utenteService;
 	
 	
 	public CartaCredito findOne(Long id) {
 		Optional<CartaCredito> user = dao.findById(id);
 		return user.get();
 		
-	}
-	
-	public CartaCredito findByNumero(String numero) {
-		
-		return dao.findByNumero(numero);
 	}
 	
 	public Iterable<CartaCredito> findAll () {
@@ -59,33 +49,15 @@ public class CartaCreditoService {
 		
 	}
 	
-	public CartaCredito associaCartaCreditoUtente(Long idCarta) {
-		
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		User user = userService.findByUsername(auth.getName());
-
+	public CartaCredito associaCartaCreditoUtente(Long idCarta,Long idUtente ) {
 		CartaCredito carta = dao.findById(idCarta).get();
+		User user = utenteService.findById(idUtente).get();
 		List<CartaCredito> listaCarteCredito = user.getCartaCredito();
 		if(listaCarteCredito==null) listaCarteCredito = new ArrayList<>();
 		listaCarteCredito.add(carta);
 		user.setCartaCredito(listaCarteCredito);
 		carta.setUser(user);
 		return dao.save(carta);
-
-	}
 	
-	public CartaCredito create(CartaCredito carta) {
-		
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		User u = userService.findByUsername(auth.getName());
-		
-		List<CartaCredito> listaCarte =  u.getCartaCredito();
-		if(listaCarte == null) listaCarte = new ArrayList<CartaCredito>();
-		
-		listaCarte.add(carta);
-		carta.setUser(u);
-		
-		return dao.save(carta);
-
 	}
 }
